6672f2e8d82d62cf1d65ebe2c71d9150d1541176
skydoves <skydoves@naver.com>
2017-10-31 21:20:36 +0900

    update RibbonTag

--- ribbonlayout/src/main/java/com/skydoves/ribbonlayout/RibbonTag.java
+++ ribbonlayout/src/main/java/com/skydoves/ribbonlayout/RibbonTag.java
@@ -5,7 +5,6 @@ import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.util.AttributeSet;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 /**
@@ -13,19 +12,16 @@ import android.widget.TextView;
  * Copyright (c) 2017 battleent rights reserved.
  */
 
-public class RibbonTag extends RelativeLayout {
+public class RibbonTag extends TextView {
 
-    private TextView ribbonTag;
-
-    private String tag_text = "";
     private int tag_textColor = Color.WHITE;
     private int tag_textSize = 11;
     private int ribbonColor = getContext().getResources().getColor(R.color.bright_lavender);
     private int ribbonRadius= 10;
-    private int padding_left = 6;
-    private int padding_top = 4;
-    private int padding_right = 6;
-    private int padding_bottom = 4;
+    private float padding_left = 6;
+    private float padding_top = 2;
+    private float padding_right = 6;
+    private float padding_bottom = 2;
 
     public RibbonTag(Context context) {
         super(context);
@@ -35,16 +31,17 @@ public class RibbonTag extends RelativeLayout {
     public RibbonTag(Context context, AttributeSet attrs) {
         super(context, attrs);
         onCreate();
+        getAttrs(attrs);
     }
 
     public RibbonTag(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         onCreate();
+        getAttrs(attrs, defStyleAttr);
     }
 
     private void onCreate() {
-        ribbonTag = new TextView(getContext());
-        ribbonTag.setBackgroundResource(R.drawable.rectangle_layout);
+        setBackgroundResource(R.drawable.rectangle_layout);
     }
 
     private void getAttrs(AttributeSet attributeSet) {
@@ -66,45 +63,42 @@ public class RibbonTag extends RelativeLayout {
     }
 
     private void setTypeArray(TypedArray typeArray) {
-        tag_text = typeArray.getString(R.styleable.RibbonTag_tag_text);
         tag_textSize = typeArray.getInt(R.styleable.RibbonTag_tag_textSize, tag_textSize);
         tag_textColor = typeArray.getColor(R.styleable.RibbonTag_tag_textColor, tag_textColor);
         ribbonColor = typeArray.getColor(R.styleable.RibbonTag_tag_ribbonColor, ribbonColor);
         ribbonRadius = typeArray.getInt(R.styleable.RibbonTag_tag_ribbonRadius, ribbonRadius);
-        padding_left = typeArray.getInt(R.styleable.RibbonTag_tag_padding_left, padding_left);
-        padding_top = typeArray.getInt(R.styleable.RibbonTag_tag_padding_top, padding_top);
-        padding_right = typeArray.getInt(R.styleable.RibbonTag_tag_padding_right, padding_right);
-        padding_bottom = typeArray.getInt(R.styleable.RibbonTag_tag_padding_bottom, padding_bottom);
+        padding_left = typeArray.getDimension(R.styleable.RibbonTag_tag_padding_left, padding_left);
+        padding_top = typeArray.getDimension(R.styleable.RibbonTag_tag_padding_top, padding_top);
+        padding_right = typeArray.getDimension(R.styleable.RibbonTag_tag_padding_right, padding_right);
+        padding_bottom = typeArray.getDimension(R.styleable.RibbonTag_tag_padding_bottom, padding_bottom);
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        createRibbonTag();
+        updateRibbonTag();
     }
 
-    private void createRibbonTag() {
-        ribbonTag.setText(tag_text);
-        ribbonTag.setTextSize(tag_textSize);
-        ribbonTag.setTextColor(tag_textColor);
-        ribbonTag.setPadding(padding_left, padding_top, padding_right, padding_bottom);
-        GradientDrawable bgShape = (GradientDrawable)ribbonTag.getBackground();
+    private void updateRibbonTag() {
+        setTextSize(tag_textSize);
+        setTextColor(tag_textColor);
+        setPadding((int)padding_left, (int)padding_top, (int)padding_right, (int)padding_bottom);
+        GradientDrawable bgShape = (GradientDrawable)getBackground();
         bgShape.setCornerRadius(ribbonRadius);
         bgShape.setColor(ribbonColor);
-        addView(ribbonTag);
     }
 
     public void setTagText(String text) {
-        this.tag_text = text;
+        setText(text);
     }
 
     public void setTagTextColor(int color) {
-        this.ribbonTag.setTextColor(color);
+        setTextColor(color);
     }
 
     public void setRibbonColor(int color) {
         this.ribbonColor = color;
-        GradientDrawable bgShape = (GradientDrawable)ribbonTag.getBackground();
+        GradientDrawable bgShape = (GradientDrawable)getBackground();
         bgShape.setColor(color);
     }
 }
